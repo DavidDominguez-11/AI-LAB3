52fa7a897697f176579452d10b56a309a132031d
David Dominguez <dominguezdavid396@gmail.com>
2026-02-05 19:53:31 -0600

    1 preprocess b clean text

--- task2.py
+++ task2.py
@@ -1,6 +1,26 @@
 print("Task 2: Filtro de Spam Bayesiano")
 
+def data(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        return file.read()
 
-with open('entrenamiento.txt', 'r', encoding='utf-8') as file:
-    contenido = file.read()
-    print(contenido)
+contenido = data('entrenamiento.txt')
+#print(contenido.lower())
+
+def get_symbols(texto):
+    symbols = []
+    for char in texto:
+        # Si NO es letra y NO es espacio (y no lo hemos guardado ya)
+        if not char.isalpha() and not char.isspace() and char not in symbols:
+            symbols.append(char)
+    return symbols
+
+simbolos = get_symbols(contenido)
+print(simbolos)
+
+def clean_text(texto):
+    for simbolo in get_symbols(texto):
+        texto = texto.replace(simbolo, "")
+    return texto.lower()
+
+print(clean_text(contenido))
