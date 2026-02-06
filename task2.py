2864bb1e627d3f58a339e8f12bf8193a21efc2d6
David Dominguez <dominguezdavid396@gmail.com>
2026-02-05 20:28:36 -0600

    data in dic keys spam ham values complete phrases

--- task2.py
+++ task2.py
@@ -4,7 +4,7 @@ def data(path):
     with open(path, 'r', encoding='utf-8') as file:
         return file.read()
 
-contenido = data('entrenamiento.txt')
+contenido = data('testdata.txt')
 #print(contenido.lower())
 
 def get_symbols(texto):
@@ -15,15 +15,15 @@ def get_symbols(texto):
             symbols.append(char)
     return symbols
 
-simbolos = get_symbols(contenido)
-print(simbolos)
+#simbolos = get_symbols(contenido)
+#print(simbolos)
 
 def clean_text(texto):
     for simbolo in get_symbols(texto):
         texto = texto.replace(simbolo, "")
     return texto.lower()
 
-print(clean_text(contenido))
+#print(clean_text(contenido))
 
 text = clean_text(contenido)
 
@@ -31,7 +31,37 @@ palabras = text.split()
 vocabulario = list()
 
 for i in palabras:
-    if i not in vocabulario:
+    if i not in vocabulario or i == 'spam' or i == 'ham':
         vocabulario.append(i)
 
-print(vocabulario)
+#print(vocabulario)
+
+# dic de ham y spam
+dic = {
+    "ham": [],
+    "spam": []
+}
+
+
+categoria_actual = None
+frase_temporal = []
+
+for palabra in vocabulario:
+    if palabra == "ham" or palabra == "spam":
+        # Si ya teníamos una categoría y palabras guardadas, las unimos y guardamos
+        if categoria_actual and frase_temporal:
+            dic[categoria_actual].append(" ".join(frase_temporal))
+        
+        # Actualizamos a la nueva categoría y reseteamos la frase temporal
+        categoria_actual = palabra
+        frase_temporal = []
+    else:
+        # Si no es etiqueta, es parte del mensaje
+        frase_temporal.append(palabra)
+
+# Al terminar el bucle, guardamos la última frase pendiente
+if categoria_actual and frase_temporal:
+    dic[categoria_actual].append(" ".join(frase_temporal))
+
+print(dic)
+
